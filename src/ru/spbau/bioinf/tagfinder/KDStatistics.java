be6060349126a9726ff53fe0ced81fd08dc913f9
Yakov Sirotkin <yasha@telamon.ru>
2011-09-23 12:34:35 +0400

    Support checking of the multiply precursorMass shifts and displaying the longest tag. http://ablab.myjetbrains.com/youtrack/issue/TagFinder-3

--- src/ru/spbau/bioinf/tagfinder/KDStatistics.java
+++ src/ru/spbau/bioinf/tagfinder/KDStatistics.java
@@ -17,12 +17,22 @@ public class KDStatistics {
         Map<Integer, Scan> scans = conf.getScans();
         KDStatistics kdStat = new KDStatistics();
         Map<KD, Integer> stat = new HashMap<KD, Integer>();
-        for (Scan scan : scans.values()) {
+        List<Integer> keys = new ArrayList<Integer>();
+        keys.addAll(scans.keySet());
+        Collections.sort(keys);
+        for (int key : keys) {
+            Scan scan = scans.get(key);
             int scanId = scan.getId();
             if (msAlignResults.containsKey(scanId)) {
-                String sequence = proteins.get(msAlignResults.get(scanId)).getSimplifiedAcids();
-                KD kd = kdStat.findKd(scan, sequence);
-                System.out.println(scanId + " " + kd.toString());
+                Integer proteinId = msAlignResults.get(scanId);
+                String sequence = proteins.get(proteinId).getSimplifiedAcids();
+                KD kd =
+                        kdStat.
+                                findKd
+                                //findKdBetweenBYAll
+                                //findKdBetweenBY
+                        (scan, sequence);
+                System.out.println(scanId + " " + kd.toString() + " " + proteinId);
                 if (stat.containsKey(kd)) {
                     stat.put(kd, 1 + stat.get(kd));
                 } else {
@@ -42,6 +52,58 @@ public class KDStatistics {
         List<Peak> peaks = scan.getPeaks();
         peaks.add(new Peak(0, 0, 0));
         peaks.add(new Peak(scan.getPrecursorMass(), 0, 0));
+        return findKd(peaks, sequence);
+    }
+
+    public KD findKdBetweenBY(Scan scan, String sequence) {
+        List<Peak> peaks = new ArrayList<Peak>();
+        peaks.addAll(scan.getPeaks());
+        double precursorMassShift = PrecursorMassShiftFinder.getPrecursorMassShift(conf, scan);
+        peaks.add(new Peak(0, 0, 0));
+        double newPrecursorMass = scan.getPrecursorMass() + precursorMassShift;
+        peaks.add(new Peak(newPrecursorMass, 0, 0));
+        for (Peak peak : scan.getPeaks()) {
+             peaks.add(peak.getYPeak(newPrecursorMass));
+        }
+        return findKd(peaks, sequence);
+    }
+
+    public KD findKdBetweenBYAll(Scan scan, String sequence) {
+        List<Double> values = PrecursorMassShiftFinder.getAllPossibleShifts(conf, scan);
+        values.add(0d);
+        values.add(PrecursorMassShiftFinder.getPrecursorMassShift(conf, scan));
+
+        Collections.sort(values);
+        KD ans = new KD(0, 0);
+        double prev = -1000;
+        int tries = 0;
+        for (double precursorMassShift : values) {
+            if (precursorMassShift - prev < 0.0001) {
+                continue;
+            }
+            prev = precursorMassShift;
+            tries++;
+            List<Peak> peaks = new ArrayList<Peak>();
+            peaks.addAll(scan.getPeaks());
+            for (Peak peak : peaks) {
+                peak.clearNext();
+            }
+            peaks.add(new Peak(0, 0, 0));
+            double newPrecursorMass = scan.getPrecursorMass() + precursorMassShift;
+            peaks.add(new Peak(newPrecursorMass, 0, 0));
+            for (Peak peak : scan.getPeaks()) {
+                 peaks.add(peak.getYPeak(newPrecursorMass));
+            }
+            KD newKD = findKd(peaks, sequence);
+            if (newKD.compareTo(ans) < 0) {
+                ans = newKD;
+            }
+        }
+        //System.out.println("tries = " + tries + " " + values.size());
+        return ans;
+    }
+
+    private KD findKd(List<Peak> peaks, String sequence) {
         Collections.sort(peaks);
 
         int n = peaks.size();
@@ -80,7 +142,7 @@ public class KDStatistics {
         }
 
         for (Peak peak : peaks) {
-            searchK(kValues, 0, peak);
+            searchK(kValues, 0, peak, new Peak[500] );
         }
 
         int k = 0;
@@ -112,25 +174,48 @@ public class KDStatistics {
         return new KD(k, d);
     }
 
-    private void searchK(int[] kValues, int len, Peak peak) {
+    private void searchK(int[] kValues, int len, Peak peak, Peak[] prefix) {
         if (peak.getMaxPrefix() >= len) {
             return;
         }
 
+        prefix[len] = peak;
+
         int componentId = peak.getComponentId();
-        if (kValues[componentId] <= len) {
+        if (kValues[componentId] < len) {
             kValues[componentId] = len;
+
+            //printPrefix(prefix, len);
+
         }
 
         for (Peak next : peak.getNext()) {
-            searchK(kValues, len + 1, next);
+            searchK(kValues, len + 1, next, prefix);
         }
 
         peak.setMaxPrefix(len);
     }
 
+    private void printPrefix(Peak[] prefix, int len) {
+        if (len < 25) {
+            return;
+        }
+        System.out.print(len + " ");
+        for (int i = 0; i < len; i++) {
+            Peak p = prefix[i];
+            System.out.print(p.getValue() + " ");
+            System.out.print(Acid.getAcid(prefix[i + 1].diff(p)).name() + " ");
+        }
+        System.out.println(prefix[len].getValue());
+    }
+
+
+    private int maxD;
+
     private int getD(Peak peak, String sequence) {
         int ans = 0;
+        Peak[] prefix = new Peak[500];
+        maxD = 0;
         for (Peak next : peak.getNext()) {
             double[] limits = conf.getEdgeLimits(peak, next);
             for (Acid acid : Acid.values()) {
@@ -138,7 +223,8 @@ public class KDStatistics {
                     String pst = acid.name();
                     int cur = sequence.indexOf(pst);
                     while (cur >=0) {
-                        int nextAns = getD(next, sequence.substring(cur+1), 1);
+                        prefix[0] = peak;
+                        int nextAns = getD(next, sequence.substring(cur+1), 1, prefix);
                         if (nextAns > ans) {
                             ans = nextAns;
                         }
@@ -150,7 +236,11 @@ public class KDStatistics {
         return ans;
     }
 
-    private int getD(Peak peak, String sequence, int matched) {
+    private int getD(Peak peak, String sequence, int matched, Peak[] prefix) {
+        prefix[matched] = peak;
+        if (matched > maxD) {
+            //printPrefix(prefix, matched);
+        }
         if (sequence.length() == 0) {
             return matched;
         }
@@ -159,7 +249,7 @@ public class KDStatistics {
             double[] limits = conf.getEdgeLimits(peak, next);
             Acid acid = Acid.getAcid(sequence.charAt(0));
             if (acid.match(limits)) {
-                int nextAns = getD(next, sequence.substring(1), matched + 1);
+                int nextAns = getD(next, sequence.substring(1), matched + 1, prefix);
                 if (nextAns > ans) {
                     ans = nextAns;
                 }
