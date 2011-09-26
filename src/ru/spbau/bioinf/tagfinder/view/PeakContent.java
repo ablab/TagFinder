e0e9c01cebf536ea4b576fed49b69813e24f8a1c
Yakov Sirotkin <yasha@telamon.ru>
2011-09-27 00:41:51 +0400

    Support colors. http://ablab.myjetbrains.com/youtrack/issue/TagFinder-4

--- src/ru/spbau/bioinf/tagfinder/view/PeakContent.java
+++ src/ru/spbau/bioinf/tagfinder/view/PeakContent.java
@@ -7,14 +7,25 @@ import ru.spbau.bioinf.tagfinder.util.XmlUtil;
 public class PeakContent implements Content {
     private Peak peak;
 
+    private String color = "white";
+
     public PeakContent(Peak peak) {
         this.peak = peak;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Peak getPeak() {
+        return peak;
     }
 
     @Override
     public Element toXml() {
         Element ans = new Element("peak");
         XmlUtil.addElement(ans, "value", peak.getValue());
+        XmlUtil.addElement(ans, "color", color);
         XmlUtil.addElement(ans, "mass", peak.getMass());
         XmlUtil.addElement(ans, "type", peak.getPeakType().name());
         return ans;
