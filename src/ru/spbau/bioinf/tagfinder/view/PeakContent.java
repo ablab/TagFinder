aeabf98375a7d9193fd38a29eb25c7efce7cf68b
Yakov Sirotkin <yasha@telamon.ru>
2011-09-27 12:39:55 +0400

    Support mod field. http://ablab.myjetbrains.com/youtrack/issue/TagFinder-4

--- src/ru/spbau/bioinf/tagfinder/view/PeakContent.java
+++ src/ru/spbau/bioinf/tagfinder/view/PeakContent.java
@@ -9,12 +9,18 @@ public class PeakContent implements Content {
 
     private String color = "white";
 
+    private double mod = 0;
+
     public PeakContent(Peak peak) {
         this.peak = peak;
     }
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public void setMod(double mod) {
+        this.mod = mod;
     }
 
     public Peak getPeak() {
@@ -26,6 +32,7 @@ public class PeakContent implements Content {
         Element ans = new Element("peak");
         XmlUtil.addElement(ans, "value", peak.getValue());
         XmlUtil.addElement(ans, "color", color);
+        XmlUtil.addElement(ans, "mod", mod);
         XmlUtil.addElement(ans, "mass", peak.getMass());
         XmlUtil.addElement(ans, "type", peak.getPeakType().name());
         return ans;
