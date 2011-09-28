56492948de02593f8d0bad32edba01fa008d5598
Yakov Sirotkin <yasha@telamon.ru>
2011-09-28 12:47:45 +0400

    Support for statistics for gapped tags. http://ablab.myjetbrains.com/youtrack/issue/TagFinder-7

--- src/ru/spbau/bioinf/tagfinder/ValidTags.java
+++ src/ru/spbau/bioinf/tagfinder/ValidTags.java
@@ -12,6 +12,8 @@ public class ValidTags {
 
     private Configuration conf;
 
+    private int gap;
+
     public ValidTags(Configuration conf) {
         this.conf = conf;
     }
@@ -29,6 +31,7 @@ public class ValidTags {
         List<Integer> keys = new ArrayList<Integer>();
         keys.addAll(scans.keySet());
         Collections.sort(keys);
+        ValidTags validTags = new ValidTags(conf);
         Set<Integer> usedProteins = new HashSet<Integer>();
         for (int key : keys) {
             Scan scan = scans.get(key);
@@ -39,12 +42,16 @@ public class ValidTags {
                     continue;
                 }
                 usedProteins.add(proteinId);
+                if (scanId == 1946) {
+                    //continue;
+                }
+
 
                 String sequence = proteins.get(proteinId).getSimplifiedAcids();
                 String reverseSequence = getReverse(sequence);
                 KDStatistics kdStatistics = new KDStatistics(conf);
-                Set<String> tags = new HashSet<String>();
-                ValidTags validTags = new ValidTags(conf);
+
+
 
 
                 List<Peak> peaks =
@@ -52,40 +59,123 @@ public class ValidTags {
                             //scan.createSpectrumWithYPeaks(PrecursorMassShiftFinder.getPrecursorMassShift(conf, scan))
                     ;
 
-                kdStatistics.generateEdges(peaks);
-                validTags.generateTags(tags, peaks);
 
-                Set<String> filteredTags = new HashSet<String>();
-                for (String tag : tags) {
-                    String reverseTag = getReverse(tag);
-                    if (!filteredTags.contains(reverseTag) || reverseTag.equals(tag)) {
-                        filteredTags.add(tag);
-                    }
-                }
-                int[][] stat = new int[1000][2];
-                int max = 0;
-                for (String tag : filteredTags) {
-                    int len = tag.length();
-                    if (len > max) {
-                        max = len;
-                    }
-                    if (sequence.contains(tag) || reverseSequence.contains(tag)) {
-                        stat[len][0]++;
-                    } else {
-                        stat[len][1]++;
-                    }
-                }
-                System.out.print(scanId + " " +  proteinId);
-                for (int i = 1; i <= max; i++) {
-                    int good = stat[i][0];
-                    int total = good + stat[i][1];
-                    if (total > 0) {
-                        System.out.print(" " + df.format((100d * good)/total));
-                    }
-                }
-                System.out.println();
+                //kdStatistics.generateEdges(peaks);
+                //printUsualTagInfo(peaks, conf, scanId, proteinId, sequence, reverseSequence);
+
+                validTags.gap = 3;
+                kdStatistics.generateGapEdges(peaks, validTags.gap);
+                validTags.printGappedTagInfo(peaks, scanId, proteinId, sequence, reverseSequence);
             }
         }
+    }
+
+    private static void printUsualTagInfo(List<Peak> peaks, Configuration conf, int scanId, Integer proteinId, String sequence, String reverseSequence) {
+        Set<String> tags = new HashSet<String>();
+        ValidTags validTags = new ValidTags(conf);
+        validTags.generateTags(tags, peaks);
+
+        Set<String> filteredTags = new HashSet<String>();
+        for (String tag : tags) {
+            String reverseTag = getReverse(tag);
+            if (!filteredTags.contains(reverseTag) || reverseTag.equals(tag)) {
+                filteredTags.add(tag);
+            }
+        }
+        int[][] stat = new int[1000][2];
+        for (String tag : filteredTags) {
+            int len = tag.length();
+            if (sequence.contains(tag) || reverseSequence.contains(tag)) {
+                stat[len][0]++;
+            } else {
+                stat[len][1]++;
+            }
+        }
+        printStat(stat, proteinId, scanId);
+    }
+
+    private void printGappedTagInfo(List<Peak> peaks, int scanId, Integer proteinId, String sequence, String reverseSequence) {
+        int[][] stat = new int[1000][2];
+        for (Peak peak : peaks) {
+            Set<Integer> starts = new HashSet<Integer>();
+            Set<Integer> reverseStarts = new HashSet<Integer>();
+            for (int i = 0; i < sequence.length(); i++) {
+                starts.add(i);
+                reverseStarts.add(i);
+            }
+            processGappedTags(stat, peak, 0, sequence, reverseSequence, starts, reverseStarts);
+        }
+        printStat(stat, proteinId, scanId);
+    }
+
+    private void processGappedTags(int[][] stat, Peak peak, int prefix, String sequence, String reverseSequence, Set<Integer> starts, Set<Integer>reverseStarts) {
+        if (prefix > 0) {
+            stat[prefix][0]++;
+        }
+        List<Peak> nextPeaks = peak.getNext();
+        for (Peak next : nextPeaks) {
+            double[] limits = conf.getEdgeLimits(peak, next);
+            Set<Integer> nextStarts = getNextStarts(sequence, starts, limits);
+            Set<Integer> nextReverseStarts = getNextStarts(reverseSequence, reverseStarts, limits);
+            if (nextStarts.size() + nextReverseStarts.size() == 0) {
+                processWrongGappedTags(stat, next, prefix + 1);
+            } else {
+                processGappedTags(stat, next, prefix + 1, sequence, reverseSequence, nextStarts, nextReverseStarts);
+            }
+        }
+    }
+
+    private void processWrongGappedTags(int[][] stat, Peak peak, int prefix) {
+        List<Peak> nextPeaks = peak.getNext();
+        stat[prefix][1]++;
+        for (Peak next : nextPeaks) {
+            processWrongGappedTags(stat, next, prefix + 1);
+        }
+    }
+
+    private Set<Integer> getNextStarts(String sequence, Set<Integer> starts, double[] limits) {
+        Set<Integer> nextStarts = new HashSet<Integer>();
+        for (int pos : starts) {
+            if (pos >= sequence.length()) {
+                continue;
+            }
+            double m = Acid.getAcid(sequence.charAt(pos)).getMass();
+            if (limits[0] < m && m < limits[1]) {
+                nextStarts.add(pos + 1);
+            }
+            if (pos  + 1 < sequence.length()) {
+                m += Acid.getAcid(sequence.charAt(pos + 1)).getMass();
+                if (limits[0] < m && m < limits[1]) {
+                    nextStarts.add(pos + 2);
+                }
+
+            }
+            if (gap > 2) {
+                if (pos  + 2 < sequence.length()) {
+                    m += Acid.getAcid(sequence.charAt(pos + 2)).getMass();
+                    if (limits[0] < m && m < limits[1]) {
+                        nextStarts.add(pos + 3);
+                    }
+
+                }
+            }
+
+        }
+        return nextStarts;
+    }
+
+    private static void printStat(int[][] stat, Integer proteinId, int scanId) {
+        System.out.print(scanId + " " +  proteinId);
+        for (int i = 1; i < stat.length; i++) {
+            int good = stat[i][0];
+            int total = good + stat[i][1];
+            if (total > 0) {
+                System.out.print(" " + df.format((100d * good)/total));
+            } else {
+                break;
+            }
+        }
+        System.out.println();
     }
 
     private static String getReverse(String tag) {
