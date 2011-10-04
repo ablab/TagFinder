0ef2b94dfbc84b673f3001e057305328f0e8ad2e
Yakov Sirotkin <yasha@telamon.ru>
2011-10-04 13:54:01 +0400

    Initial version. http://ablab.myjetbrains.com/youtrack/issue/TagFinder-8

---
+++ src/ru/spbau/bioinf/tagfinder/Consts.java
@@ -0,0 +1,15 @@
+package ru.spbau.bioinf.tagfinder;
+
+public class Consts {
+
+    public static double WATER = 18.010565;
+    public static double AMMONIA = 17.0265;
+    public static double OXYGEN = 15.9949;
+    public static double H = 1.007825;
+    public static double C = 12;
+    public static double CO = OXYGEN + C;
+    public static double N = 14.0030740048;
+    public static double NH = N + H;
+    public static double OH = OXYGEN + H;
+
+}
