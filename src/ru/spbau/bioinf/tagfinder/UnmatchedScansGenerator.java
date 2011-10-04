cd8ed8f4953e0eaa30808aaf7da1f6c98666a8f8
Yakov Sirotkin <yasha@telamon.ru>
2011-10-04 14:25:43 +0400

    Support removing of the shared peaks. http://ablab.myjetbrains.com/youtrack/issue/TagFinder-8

--- src/ru/spbau/bioinf/tagfinder/UnmatchedScansGenerator.java
+++ src/ru/spbau/bioinf/tagfinder/UnmatchedScansGenerator.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import java.util.Set;
 
 public class UnmatchedScansGenerator {
+    private static final String TAGS_MODE = "mod2";
+    private static final String SHARED_MODE = "mod3";
     private Configuration conf;
 
     public UnmatchedScansGenerator(Configuration conf) {
@@ -16,7 +18,12 @@ public class UnmatchedScansGenerator {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration(args);
-        Configuration conf2 = new Configuration(args, "mod2");
+        new UnmatchedScansGenerator(conf).generateUnmatchedScans(args, conf, SHARED_MODE);
+    }
+
+    public void generateUnmatchedScans(String[] args, Configuration conf, String mod) throws Exception {
+        Configuration conf2 = new Configuration(args, mod);
+
         List<Protein> proteins = conf.getProteins();
         Map<Integer,Integer> msAlignResults = conf.getMSAlignResults();
         Map<Integer, Scan> scans = conf.getScans();
@@ -24,45 +31,23 @@ public class UnmatchedScansGenerator {
         keys.addAll(scans.keySet());
         Collections.sort(keys);
 
-        KDStatistics kdStatistics = new KDStatistics(conf);
         for (int key : keys) {
             Scan scan = scans.get(key);
             int scanId = scan.getId();
             if (msAlignResults.containsKey(scanId)) {
                 int proteinId = msAlignResults.get(scanId);
-                String sequence = proteins.get(proteinId).getSimplifiedAcids();
-                String reverseSequence = ValidTags.getReverse(sequence);
 
                 List<Peak> peaks = scan.createStandardSpectrum();
-                kdStatistics.generateEdges(peaks);
-                Set<Peak> removed = new HashSet<Peak>();
-                for (Peak p1 : peaks) {
-                    for (Peak p2 : p1.getNext()) {
-                        for (Acid a1 : Acid.values()) {
-                            if (a1.match(conf.getEdgeLimits(p1, p2))) {
-                                for (Peak p3 : p2.getNext()) {
-                                    for (Acid a2 : Acid.values()) {
-                                        if (a2.match(conf.getEdgeLimits(p2, p3))) {
-                                            for (Peak p4 : p3.getNext()) {
-                                                for (Acid a3 : Acid.values()) {
-                                                    if (a3.match(conf.getEdgeLimits(p3, p4))) {
-                                                        String tag = a1.name() + a2.name() + a3.name();
-                                                        if (sequence.contains(tag) || reverseSequence.contains(tag)) {
-                                                            removed.add(p1);
-                                                            removed.add(p2);
-                                                            removed.add(p3);
-                                                            removed.add(p4);
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+
+                String sequence = proteins.get(proteinId).getSimplifiedAcids();
+                Set<Peak> removed = null;
+                if (TAGS_MODE.equals(mod)) {
+                    removed = getRemovedPeaksByTags(peaks, sequence);
+                } if (SHARED_MODE.equals(mod)) {
+                    removed = getRemovedPeaksByShared(peaks, sequence, scan.getPrecursorMass());
                 }
+
+
                 if (removed.size() > 0) {
                     System.out.println(scanId + " removed.size() = " + removed.size());
                     for (Peak peak : removed) {
@@ -80,5 +65,68 @@ public class UnmatchedScansGenerator {
                 }
             }
         }
+    }
+
+    private Set<Peak> getRemovedPeaksByShared(List<Peak> peaks, String sequence, double precursorMass) {
+        Set<Peak> removed = new HashSet<Peak>();
+        double[] protein = ShiftEngine.getSpectrum(sequence);
+        List<Double> shifts = ShiftEngine.getShifts(peaks, precursorMass, protein);
+        double bestShift = 0;
+        double bestScore = 0;
+        double[] spectrum = ShiftEngine.getSpectrum(peaks, precursorMass);
+        for (Double shift : shifts) {
+            double nextScore = ShiftEngine.getScore(spectrum, protein, shift);
+            if (nextScore > bestScore) {
+                bestScore = nextScore;
+                bestShift = shift;
+            }
+        }
+        double[] mod = new double[]{0, -1, +1, Consts.WATER, -Consts.WATER, Consts.AMMONIA};
+        for (Peak peak : peaks) {
+            for (double dv : mod) {
+                double modMass = peak.getMass() + dv;
+                double v1 = modMass + bestShift;
+                double v2 = precursorMass - modMass + bestShift;
+                if (ShiftEngine.contains(protein, v1, v2)) {
+                    removed.add(peak);
+                    break;
+                }
+            }
+        }
+        return removed;
+    }
+    private Set<Peak> getRemovedPeaksByTags(List<Peak> peaks, String sequence) {
+        String reverseSequence = ValidTags.getReverse(sequence);
+
+        GraphUtil.generateEdges(conf, peaks);
+        Set<Peak> removed = new HashSet<Peak>();
+        for (Peak p1 : peaks) {
+            for (Peak p2 : p1.getNext()) {
+                for (Acid a1 : Acid.values()) {
+                    if (a1.match(conf.getEdgeLimits(p1, p2))) {
+                        for (Peak p3 : p2.getNext()) {
+                            for (Acid a2 : Acid.values()) {
+                                if (a2.match(conf.getEdgeLimits(p2, p3))) {
+                                    for (Peak p4 : p3.getNext()) {
+                                        for (Acid a3 : Acid.values()) {
+                                            if (a3.match(conf.getEdgeLimits(p3, p4))) {
+                                                String tag = a1.name() + a2.name() + a3.name();
+                                                if (sequence.contains(tag) || reverseSequence.contains(tag)) {
+                                                    removed.add(p1);
+                                                    removed.add(p2);
+                                                    removed.add(p3);
+                                                    removed.add(p4);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return removed;
     }
 }
