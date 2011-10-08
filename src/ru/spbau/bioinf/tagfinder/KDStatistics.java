1011d246f7b7e692c38e732ad6ec3b18a617cf20
Yakov Sirotkin <yasha@Yakovs-MacBook-Air.local>
2011-10-08 12:44:20 +0400

    Generate gapped tags using GraphUtil. http://ablab.myjetbrains.com/youtrack/issue/TagFinder-7

--- src/ru/spbau/bioinf/tagfinder/KDStatistics.java
+++ src/ru/spbau/bioinf/tagfinder/KDStatistics.java
@@ -173,41 +173,6 @@ public class KDStatistics {
         return new KD(k, d);
     }
 
-    public void generateGapEdges(List<Peak> peaks, int allow) {
-        int n = peaks.size();
-        List<Double> masses = new ArrayList<Double>();
-        Acid[] values = Acid.values();
-        for (Acid acid : values) {
-            masses.add(acid.getMass());
-        }
-        for (int i = 0; i < values.length; i++) {
-            Acid a1 = values[i];
-            for (int j = i + 1; j < values.length; j++) {
-                Acid a2 = values[j];
-                masses.add(a1.getMass() + a2.getMass());
-                if (allow > 2) {
-                    for (int k = j + 1; k < values.length; k++) {
-                        Acid a3 = values[k];
-                        masses.add(a1.getMass() + a2.getMass() + a3.getMass());
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            Peak peak = peaks.get(i);
-            for (int j = i+1; j < n; j++) {
-                Peak next =  peaks.get(j);
-                double[] limits = conf.getEdgeLimits(peak, next);
-                for (double mass : masses) {
-                    if (limits[0] < mass && limits[1] > mass) {
-                        peak.addNext(next);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
     private void searchK(int[] kValues, int len, Peak peak, Peak[] prefix) {
         if (peak.getMaxPrefix() >= len) {
             return;
