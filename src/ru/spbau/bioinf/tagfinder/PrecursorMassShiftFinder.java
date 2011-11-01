72318e4fa02613cfd714900e902c4226d21578c9
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2011-11-01 18:17:00 +0300

    Add more ideas for finding precursorMass shifts .  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-28

--- src/ru/spbau/bioinf/tagfinder/PrecursorMassShiftFinder.java
+++ src/ru/spbau/bioinf/tagfinder/PrecursorMassShiftFinder.java
@@ -1,5 +1,6 @@
 package ru.spbau.bioinf.tagfinder;
 
+import java.util.Collections;
 import ru.spbau.bioinf.tagfinder.util.StatisticsUtil;
 
 import java.io.IOException;
@@ -35,7 +36,38 @@ public class PrecursorMassShiftFinder {
                 double delta = sum - precursorMass;
                 double absDelta = Math.abs(delta);
                 for (Acid acid : Acid.values()) {
-                    double error = conf.getPpmCoef() * 3 * sum / 2;
+                    double error = conf.getPpmCoef() * 3 * sum;
+                    double errorU = conf.getPpmCoef() * sum/2;
+                    if (acid.match(new double[]{absDelta - error,  absDelta + error})) {
+                        if (acid.match(new double[]{absDelta - errorU,  absDelta + errorU})) {
+                            continue;
+                        }
+                        if (delta > 0) {
+                            values.add(delta - acid.getMass());
+                        } else {
+                            values.add(delta + acid.getMass());
+                        }
+                    }
+                }
+            }
+        }
+        return values;
+    }
+
+    public static List<Double> getAllPossibleShifts2(Configuration conf, Scan scan) {
+        List<Peak> peaks = scan.getPeaks();
+        double precursorMass = scan.getPrecursorMass();
+        double error = precursorMass * conf.getPpmCoef() * 3;
+        int n = peaks.size();
+        List<Double> values = new ArrayList<Double>();
+        for (int i = 0; i < n; i++) {
+            Peak peak = peaks.get(i);
+            for (int j = i+1; j < n; j++) {
+                Peak next =  peaks.get(j);
+                double sum = peak.getValue() + next.getValue();
+                double delta = sum - precursorMass;
+                double absDelta = Math.abs(delta);
+                for (Acid acid : Acid.values()) {
                     if (acid.match(new double[]{absDelta - error,  absDelta + error})) {
                         if (delta > 0) {
                             values.add(delta - acid.getMass());
@@ -47,6 +79,60 @@ public class PrecursorMassShiftFinder {
             }
         }
         return values;
+    }
+
+    public static double getPrecursorMassShiftForMoreEdges(Configuration conf, Scan scan) {
+        List<Peak> peaks = scan.getPeaks();
+        double precursorMass = scan.getPrecursorMass();
+        double error = precursorMass * conf.getPpmCoef() * 3;
+        int n = peaks.size();
+        List<double[]> limits = new ArrayList<double[]>();
+        List<Double> points = new ArrayList<Double>();
+        for (int i = 0; i < n; i++) {
+            Peak peak = peaks.get(i);
+            for (int j = i+1; j < n; j++) {
+                Peak next =  peaks.get(j);
+                double sum = peak.getValue() + next.getValue();
+                double dError = sum * conf.getPpmCoef() / 2;
+                double delta = sum - precursorMass;
+                double absDelta = Math.abs(delta);
+                for (Acid acid : Acid.values()) {
+                    if (acid.match(new double[]{absDelta - error,  absDelta + error})) {
+                        double opt = delta > 0 ? delta - acid.getMass() : delta + acid.getMass();
+                        points.add(opt - dError);
+                        points.add(opt + dError);
+                        limits.add(new double[] {opt - dError, opt + dError});
+                    }
+                }
+            }
+        }
+        Collections.sort(points);
+        double ans = 0;
+        int score = 0000;
+        points.clear();
+        double min = - precursorMass * conf.getPpmCoef() * 3;
+        double delta = -min/100000;
+        for (int i = 0; i < 200000; i++) {
+            double p = min + delta * i;
+        //for (int i = 0; i < points.size() - 1; i++) {
+        //    double p = (points.get(i) + points.get(i + 1)) / 2;
+            if (Math.abs(p) > conf.getPpmCoef() * 3 * precursorMass) {
+                continue;
+            }
+            int nextScore = 0;
+            for (double[] limit : limits) {
+                if (limit[0] < p && p < limit[1]) {
+                    nextScore++;
+                }
+            }
+            if (nextScore > 0) {
+                if (nextScore > score || (nextScore == score && (Math.abs(ans) > Math.abs(p)))) {
+                    ans = p;
+                    score = nextScore;
+                }
+            }
+        }
+        return ans;
     }
 
     public static double getPrecursorMassShift(Configuration conf, Scan scan) {
