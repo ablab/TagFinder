4680feb423a8335a34c1d3ce1ef924761b944194
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2011-11-17 18:54:31 +0300

    Generate  E-values for new matches.  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-32

--- src/ru/spbau/bioinf/tagfinder/TagProteinGenerator.java
+++ src/ru/spbau/bioinf/tagfinder/TagProteinGenerator.java
@@ -1,10 +1,8 @@
 package ru.spbau.bioinf.tagfinder;
 
-import ru.spbau.bioinf.tagfinder.util.ReaderUtil;
+import edu.ucsd.msalign.align.prsm.PrSM;
+import java.util.HashMap;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -14,27 +12,17 @@ import java.util.Set;
 
 public class TagProteinGenerator {
 
-    public static final int DEPTH = 7;
-    private Configuration conf;
+    private static Configuration conf = new Configuration(new String[]{});
 
-    private PrintWriter out;
+    private static Map<Integer,Integer> msAlignResults;
 
-
-    public TagProteinGenerator(Configuration conf) throws IOException{
-        this.conf = conf;
-        out = ReaderUtil.createOutputFile(new File("tags-bad-match-" + DEPTH +  ".txt"));
-    }
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration(args//, UnmatchedScansGenerator.SHARED_MODE
-         );
-        TagProteinGenerator generator = new TagProteinGenerator(conf);
-        generator.generateTags();
-    }
-
-    private void generateTags() throws Exception {
+        conf = new Configuration(args//, UnmatchedScansGenerator.SHARED_MODE
+        );
         List<Protein> proteins = conf.getProteins();
-        Map<Integer,Integer> msAlignResults = conf.getBadMSAlignResults();
+        Map<Integer, Integer> badMSAlignResults = conf.getBadMSAlignResults();
+        msAlignResults = conf.getMSAlignResults();
         Map<Integer, Scan> scans = conf.getScans();
         List<Integer> keys = new ArrayList<Integer>();
         keys.addAll(scans.keySet());
@@ -44,70 +32,136 @@ public class TagProteinGenerator {
             sequences.add(protein.getSimplifiedAcids());
         }
 
+        EValueAdapter.init(conf);
+
 
         for (int key : keys) {
             Scan scan = scans.get(key);
             int scanId = scan.getId();
-            int matchedProteinId = - 1;
+            int oldProteinId = -1;
 
             if (msAlignResults.containsKey(scanId)) {
-                matchedProteinId = msAlignResults.get(scanId);
-            }
-            if (matchedProteinId == -1) {
+                //if (!badMSAlignResults.containsKey(scanId)) {
                 continue;
+                //} else {
+                //    System.out.println(scanId + " in both");
+                //}
             }
 
-            System.out.println("matchedProteinId = " + matchedProteinId);
+            if (badMSAlignResults.containsKey(scanId)) {
+                oldProteinId = badMSAlignResults.get(scanId);
+            }
+            if (oldProteinId == -1) {
+                //continue;
+            }
 
-            List<Peak> peaks = scan.createStandardSpectrum();
-            //scan.createSpectrumWithYPeaks(PrecursorMassShiftFinder.getPrecursorMassShift(conf, scan));
+            //System.out.println("matchedProteinId = " + matchedProteinId);
+
+            List<Peak> peaks = //scan.createStandardSpectrum();
+                    scan.createSpectrumWithYPeaks(PrecursorMassShiftFinder.getPrecursorMassShiftForMoreEdges(conf, scan));
             GraphUtil.generateEdges(conf, peaks);
-            generateFiveAcidsTags(peaks, sequences, matchedProteinId, scan.getId());
+            generateFiveAcidsTags(peaks, sequences, oldProteinId, scan);
         }
-        out.close();
     }
 
-    private Set<String> used = new HashSet<String>();
 
-    public void generateFiveAcidsTags(List<Peak> peaks, List<String> sequences, int matchedProteinid, int scanId) {
-        used.clear();
+    public static void generateFiveAcidsTags(List<Peak> peaks, List<String> sequences, int oldProteinId, Scan scan) throws Exception {
+        List<Set<String>> allTags = new ArrayList<Set<String>>();
+        for (int depth = 10; depth >= 5; depth--) {
+            Set<String> tags = getTags(peaks, depth);
+            if (tags.size() > 0) {
+                allTags.add(tags);
+            }
+        }
+
+        HashMap<String, Integer> stat = new HashMap<String, Integer>();
+        for (Set<String> tagSet : allTags) {
+            for (String tag : tagSet) {
+                int count = 0;
+                for (int i = 0; i < sequences.size(); i++) {
+                    String seq = sequences.get(i);
+                    if (seq.contains(tag)) {
+                        count++;
+                    }
+                }
+                stat.put(tag, count);
+            }
+        }
+
+
+
+        for (int proteinId = 0; proteinId < sequences.size(); proteinId++) {
+            String seq = sequences.get(proteinId);
+            Set<String> found = new HashSet<String>();
+            boolean isMatch = false;
+            for (Set<String> tagSet : allTags) {
+                for (String tag : tagSet) {
+                    boolean isSubTag = false;
+                    for (String oldTag : found) {
+                        if (oldTag.contains(tag)) {
+                            isSubTag = true;
+                            break;
+                        }
+                    }
+                    if (isSubTag) {
+                        continue;
+                    }
+                    if (seq.contains(tag)) {
+                        found.add(tag);
+                        if (found.size() > 2 || tag.length() >= 5) {
+                            isMatch = true;
+                        }
+                    }
+                }
+            }
+            if (isMatch) {
+                Set<String> bestTags = allTags.get(0);
+                int maxTag = bestTags.iterator().next().length();
+                String scanText = "Scan " + scan.getId() + " with " + bestTags.size() + " " + maxTag + "-aa tags ";
+                if (oldProteinId >= 0) {
+                    scanText += " matched to protein " + oldProteinId;
+                }
+
+                int otherMatches = 0;
+                for (Map.Entry<Integer, Integer> entry : msAlignResults.entrySet()) {
+                    if (entry.getValue().equals(proteinId)) {
+                        otherMatches++;
+                    }
+                }
+
+                if (otherMatches == 0) {
+                    //continue;
+                }
+                PrSM prsm = EValueAdapter.getBestEValue(scan, proteinId);
+                double eValue = prsm == null? 9E10 : prsm.getEValue();
+
+                System.out.println(scan.getId() + " " + oldProteinId + " " + proteinId + " " + eValue);
+            }
+        }
+
+    }
+
+    public static Set<String> getTags(List<Peak> peaks, int depth) {
+        Set<String> tags = new HashSet<String>();
         for (Peak peak : peaks) {
-            generateFiveAcidsTags(peak, "", sequences, matchedProteinid, scanId);
+            generateTags(tags, peak, "", depth);
         }
+        return tags;
     }
 
-    public void generateFiveAcidsTags(Peak peak, String prefix, List<String> sequences, int matchedProteinid, int scanId) {
-        if (prefix.length() == DEPTH) {
-            String reverse = new StringBuilder(prefix).reverse().toString();
-            if (used.contains(prefix) || used.contains(reverse)) {
-                return;
-            }
-            for (int i = 0; i < sequences.size(); i++) {
-                String s = sequences.get(i);
-                int index = - 1 ;
-                if (s.contains(prefix)) {
-                    while ((index = s.indexOf(prefix, index + 1)) >= 0) {
-                        out.println(scanId + " " + matchedProteinid + " " + i + " " + prefix + " " + index + " " + peak.getMass() );
-                    }
-                }
-
-                if (s.contains(reverse)) {
-                    while ((index = s.indexOf(reverse, index + 1)) >= 0) {
-                        out.println(scanId + " " + matchedProteinid + "R " + i + " " + prefix + " " + index + " " + peak.getMass());
-                    }
-                }
-
-            }
+    public static void generateTags(Set<String> used, Peak peak, String prefix, int depth) {
+        if (prefix.length() == depth) {
             used.add(prefix);
         } else {
             for (Peak next : peak.getNext()) {
                 for (Acid acid : Acid.values()) {
-                    if (acid.match(conf.getEdgeLimits(peak, next))){
-                        generateFiveAcidsTags(next, prefix + acid.name(), sequences, matchedProteinid, scanId);
+                    if (acid.match(conf.getEdgeLimits(peak, next))) {
+                        generateTags(used, next, prefix + acid.name(), depth);
                     }
                 }
             }
 
         }
     }
+
 }
