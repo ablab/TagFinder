20b48de22037d5b4c77fc4245f8874d30e91fd75
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2011-11-25 15:13:12 +0400

    Generate  special spectra for fats PrSM search.  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-36

--- src/ru/spbau/bioinf/tagfinder/Protein.java
+++ src/ru/spbau/bioinf/tagfinder/Protein.java
@@ -1,5 +1,7 @@
 package ru.spbau.bioinf.tagfinder;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.jdom.Element;
 import ru.spbau.bioinf.tagfinder.util.XmlUtil;
 
@@ -40,5 +42,57 @@ public class Protein {
             simplifiedAcids = sequence.replaceAll("L", "I").replaceAll("Z", "Q").replaceAll("B", "E").replaceAll("X", "I");
         }
         return simplifiedAcids;
+    }
+
+    private double[] yends = null;
+
+    public double[] getYEnds() {
+        if (yends != null) {
+            return yends;
+        }
+        List<Double> ans = new ArrayList<Double>();
+        double m = 0;
+        String s = getSimplifiedAcids();
+        for (int cur = s.length() - 1; cur >= 0; cur--) {
+            Acid acid = Acid.getAcid(s.charAt(cur));
+            if (acid != null) {
+                m += acid.getMass();
+                ans.add(m);
+            } else {
+                break;
+            }
+        }
+        yends = new double[ans.size()];
+        for (int i = 0; i < yends.length; i++) {
+            yends[i] = ans.get(i);
+        }
+
+        return yends;
+    }
+
+    private double[] bends = null;
+
+    public double[] getBEnds() {
+        if (bends != null) {
+            return bends;
+        }
+        List<Double> ans = new ArrayList<Double>();
+        double m = 0;
+        String s = getSimplifiedAcids();
+        for (int cur = 1; cur < s.length(); cur++) {
+            Acid acid = Acid.getAcid(s.charAt(cur));
+            if (acid != null) {
+                m += acid.getMass();
+                ans.add(m);
+            } else {
+                break;
+            }
+        }
+        bends = new double[ans.size()];
+        for (int i = 0; i < bends.length; i++) {
+            bends[i] = ans.get(i);
+        }
+
+        return bends;
     }
 }
