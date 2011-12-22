82c5090ac6f32a64382ddd84faaf579282131cb3
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2011-12-22 17:59:38 +0400

    Initial version.  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-34

---
+++ plot.cmd
@@ -0,0 +1,18 @@
+call gnuplot  plots/l-mono.gpl 
+call gnuplot  plots/l-mono-correct.gpl 
+call gnuplot  plots/l-mono-correct-rel.gpl 
+call gnuplot  plots/longest-correct-l.gpl 
+call gnuplot  plots/proper-l-tags.gpl 
+call gnuplot  plots/correct-vs-proper-l-tags.gpl 
+call gnuplot  plots/correct-l-tags-err.gpl 
+call gnuplot  plots/correct-l-tags-adv.gpl 
+call gnuplot  plots/correct-l-tags.gpl 
+call gnuplot  plots/avg-top-scoring.gpl 
+call gnuplot  plots/all-top-scoring.gpl 
+
+
+call gnuplot  plots/1aa.gpl 
+call gnuplot  plots/2aa.gpl 
+call gnuplot  plots/3aa.gpl 
+
+call gnuplot  plots/longest-correct-l_histogram.gpl 
