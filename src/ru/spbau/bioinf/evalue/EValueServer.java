21e47d81b1570da93594de05ba45030c3e8ede71
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2012-01-19 14:04:29 +0400

    Support alignment.  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-43

--- src/ru/spbau/bioinf/evalue/EValueServer.java
+++ src/ru/spbau/bioinf/evalue/EValueServer.java
@@ -2,22 +2,36 @@ package ru.spbau.bioinf.evalue;
 
 
 import edu.ucsd.msalign.align.prsm.PrSM;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.stream.StreamSource;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.Serializer;
+import net.sf.saxon.s9api.XdmNode;
+import net.sf.saxon.s9api.XsltCompiler;
+import net.sf.saxon.s9api.XsltTransformer;
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.jdom.Element;
+import org.jdom.transform.JDOMSource;
+import ru.spbau.bioinf.palign.Aligner;
 import ru.spbau.bioinf.tagfinder.Configuration;
 import ru.spbau.bioinf.tagfinder.EValueAdapter;
+import ru.spbau.bioinf.tagfinder.Protein;
 import ru.spbau.bioinf.tagfinder.Scan;
 
 
@@ -25,10 +39,10 @@ public class EValueServer extends AbstractHandler {
     private static Map<Integer, Scan> scans;
 
     private static Logger log = Logger.getLogger(EValueServer.class);
+    private static List<Protein> proteins;
 
     public static void main(String[] args) throws Exception {
         init(args);
-        DbUtil.initDatabase();
 
         Server server = new Server(8080);
         server.setHandler(new EValueServer());
@@ -39,6 +53,8 @@ public class EValueServer extends AbstractHandler {
         Configuration conf = new Configuration(args);
         EValueAdapter.init(conf);
         scans = conf.getScans();
+        proteins = conf.getProteins();
+        DbUtil.initDatabase();
     }
 
     public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException, ServletException {
@@ -46,7 +62,27 @@ public class EValueServer extends AbstractHandler {
         try {
             int scanId = Integer.parseInt(httpServletRequest.getParameter("scanId"));
             int proteinId = Integer.parseInt(httpServletRequest.getParameter("proteinId"));
-            out.println(Double.toString(getEvalue(scanId, proteinId)));
+            String path = request.getPathInfo();
+            if (path.endsWith("evalue")) {
+                out.println(Double.toString(getEvalue(scanId, proteinId)));
+            } else if (path.endsWith("align")) {
+                Element alignment = Aligner.findAlignment(scans.get(scanId), proteins.get(proteinId)).toXml();
+                Processor processor = new Processor(false);
+                XdmNode source = processor.newDocumentBuilder().build(new JDOMSource(alignment));
+                Serializer ser = new Serializer();
+                ser.setOutputProperty(Serializer.Property.METHOD, "html");
+                ser.setOutputProperty(Serializer.Property.INDENT, "yes");
+                ser.setOutputWriter(out);
+
+                XsltCompiler comp = processor.newXsltCompiler();
+                XsltTransformer trans = comp.compile(new StreamSource(
+                        new InputStreamReader(new FileInputStream(new File("xsl", "alignment.xsl")), "UTF-8"))).load();
+                trans.setInitialContextNode(source);
+                trans.setDestination(ser);
+                trans.transform();
+
+                //XmlUtil.outputter.output(alignment, out);
+            }
         } catch (Throwable e) {
             log.debug("Error executing request", e);
             out.println("Error: " + e.getMessage());
