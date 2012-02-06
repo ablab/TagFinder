266418a506b0fbc80dd2f339b7f9840d3fe88511
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2012-02-06 10:43:15 +0400

    Support atom count data.  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-45

--- src/ru/spbau/bioinf/tagfinder/Acid.java
+++ src/ru/spbau/bioinf/tagfinder/Acid.java
@@ -3,33 +3,41 @@ package ru.spbau.bioinf.tagfinder;
 import java.util.HashMap;
 
 public enum Acid {
-    G(57.02146),
-    A(71.03711),
-    S(87.03203),
-    P(97.05276),
-    V(99.06841),
-    T(101.04768),
-    C(103.00919 + 57.021464), //CamC is a modification of cysteine
-    I(113.08406),
-    //L(113.08406),
-    N(114.04293),
-    D(115.02694),
-    R(156.10111),
-    Q(128.05858),
-    K(128.09496),
-    E(129.04259),
-    M(131.04049),
-    H(137.05891),
-    F(147.06841),
+
+    G(57.02146, 2, 3, 1, 1, 0),
+    A(71.03711, 3, 5, 1, 1, 0),
+    S(87.03203, 3, 5, 1, 2, 0),
+    P(97.05276, 5, 7, 1, 1, 0),
+    V(99.06841, 5, 9, 1, 1, 0),
+    T(101.04768, 4, 7, 1, 2, 0),
+    C(103.00919 + 57.021464, 5, 8, 2, 2, 1), //CamC is a modification of cysteine
+    I(113.08406, 6, 11, 1, 1, 0),
+    //L(113.08406, 6, 13, 1, 1, 0),
+    N(114.04293, 4, 6, 2, 2, 0),
+    D(115.02694, 4, 5, 1, 3, 0),
+    R(156.10111, 6, 12, 4, 1, 0),
+    Q(128.05858, 5, 8, 2, 2, 0),
+    K(128.09496, 6, 12, 2, 1, 0),
+    E(129.04259, 5, 7, 1, 3, 0),
+    M(131.04049, 5, 9, 1, 1, 1),
+    H(137.05891, 6, 7, 3, 1, 0),
+    F(147.06841, 9, 9, 1, 1, 0),
     //U(150.953636),  //too rare
-    Y(163.06333),
-    W(186.07931);
+    Y(163.06333, 9, 9, 1, 2, 0),
+    W(186.07931, 11, 10, 2, 1, 0);
     //O(237.147727); //too rare
 
     private double mass;
 
-    Acid(double mass) {
+    private final int[] atomCount;
+
+    public int[] getAtomCount() {
+        return atomCount;
+    }
+
+    private Acid(double mass, int carbon, int hydrogen, int nitrogen, int oxygen, int sulfur) {
         this.mass = mass;
+        atomCount = new int[]{carbon, hydrogen, nitrogen, oxygen, sulfur};
     }
 
     public double getMass() {
