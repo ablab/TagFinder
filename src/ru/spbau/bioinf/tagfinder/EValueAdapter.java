1f1abf156d20e0c7bac14edf96380e422c1534a3
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2012-02-17 11:43:02 +0400

    Switch to MS-Align 0.7.  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-47

--- src/ru/spbau/bioinf/tagfinder/EValueAdapter.java
+++ src/ru/spbau/bioinf/tagfinder/EValueAdapter.java
@@ -1,22 +1,26 @@
 package ru.spbau.bioinf.tagfinder;
 
+import edu.ucsd.msalign.align.MsAlign;
+import edu.ucsd.msalign.align.PropertyUtil;
 import edu.ucsd.msalign.align.idevalue.IdEValue;
 import edu.ucsd.msalign.align.prsm.PrSM;
 import edu.ucsd.msalign.seq.MsAlignSeq;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 public class EValueAdapter {
 
-    private static IdEValue eValueCalculator;
+    private static MsAlign align;
 
     public static String getVersion() {
-        return "zero0.6";
+        return "zero0.7";
     }
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration(args);
         init(conf);
-        Map<Integer,Scan> scans = conf.getScans();
+        Map<Integer, Scan> scans = conf.getScans();
         conf.getMSAlignResults();
         for (int i = 0; i < input.length; i += 2) {
             Scan scan = scans.get(input[i]);
@@ -27,67 +31,49 @@ public class EValueAdapter {
     }
 
     public static void init(Configuration conf) throws Exception {
-        final Properties properties = new Properties();
-        properties.setProperty("databaseFileName", conf.getProteinDatabaseFile().getCanonicalPath());
-        properties.setProperty("useDecoyDb", "false");
-        properties.setProperty("activation", "CID");
-        properties.setProperty("cysteineProtection", "C57");
-        properties.setProperty("reportNumber", "1");
-        properties.setProperty("shiftNumber", "2");
-        properties.setProperty("errorTolerance", "15");
-        properties.setProperty("eValueThreshold", "0.01");
-        eValueCalculator = new IdEValue();
-        eValueCalculator.init(properties);
+        conf.getScans();
+        Properties prop = PropertyUtil.getDefaultProperties();
+        prop.put("databaseFileName", conf.getProteinDatabaseFile().getAbsolutePath());
+        prop.put("spectrumFileName", conf.getMsalignFile().getAbsolutePath());
+        prop.put("activation", "CID");
+        prop.put("cysteineProtection", "C57");
+        align = new MsAlign(prop);
+        //align.prepare();
+        align.readSpectra();
+
     }
 
-    public static synchronized PrSM[][][] calculateEValue(Scan scan, int proteinId) throws Exception {
-        PrSM[][][] prsms;
-        synchronized (eValueCalculator) {
-            MsAlignSeq msAlignSeq = eValueCalculator.getSeqs()[proteinId];
-            prsms = eValueCalculator.getPrsms(scan.getMsDeconvPeaks(), msAlignSeq);
+
+    public static synchronized List<PrSM[]> calculateEValue(Scan scan, int... proteinIds) throws Exception {
+        List<PrSM[]> prsms;
+        synchronized (align) {
+            align.updateSeqs(proteinIds);
+            prsms = align.getPrsms(scan.getId());
         }
         return prsms;
     }
 
-    private static void output(PrSM prsms[][][]) {
+    private static void output(List<PrSM[]> prsms) {
         if (prsms != null) {
-            for (int i = 0; i < prsms.length; i++) {
-                if (prsms[i] != null) {
-                    for (int j = 0; j < 4; j++) {
-                        if (prsms[i][j] != null) {
-                            for (int k = 0; k < prsms[i][j].length; k++) {
-                                if (prsms[i][j][k] != null) {
-                                    PrSM prsm = prsms[i][j][k];
-                                    System.out.println("Shift " + i + " Alignment type " + j + " score " + prsm.getUniqueScr() + " evalue " + prsm.getEValue());
-                                }
-                            }
-                        }
-                    }
+            for (PrSM[] prsmA : prsms) {
+                for (PrSM prsm : prsmA) {
+                    System.out.println("Shift " + prsm.getInterShiftNum() + " Alignment type " + prsm.getEValueAlignType() + " score " + prsm.getUniqueScr() + " evalue " + prsm.getEValue());
                 }
             }
         }
     }
 
-    public static PrSM getBestEValue(Scan scan, int proteinId) throws Exception  {
-        PrSM prsms[][][] = calculateEValue(scan, proteinId);
+    public static PrSM getBestEValue(Scan scan, int proteinId) throws Exception {
+        List<PrSM[]> prsms = calculateEValue(scan, proteinId);
         double best = 9E100;
         PrSM ans = null;
         if (prsms != null) {
-            for (int i = 0; i < prsms.length; i++) {
-                if (prsms[i] != null) {
-                    for (int j = 0; j < 4; j++) {
-                        if (prsms[i][j] != null) {
-                            for (int k = 0; k < prsms[i][j].length; k++) {
-                                if (prsms[i][j][k] != null) {
-                                    PrSM prsm = prsms[i][j][k];
-                                    double newValue = prsm.getEValue();
-                                    if (newValue < best) {
-                                        best = newValue;
-                                        ans = prsm;
-                                    }
-                                }
-                            }
-                        }
+            for (PrSM[] prsmA : prsms) {
+                for (PrSM prsm : prsmA) {
+                    double newValue = prsm.getEValue();
+                    if (newValue < best) {
+                        best = newValue;
+                        ans = prsm;
                     }
                 }
             }
@@ -95,54 +81,54 @@ public class EValueAdapter {
         return ans;
     }
 
-    private static int[] input = new int[] {
-1082,  4375,
-1082, 392,
-1082, 1007,
-1082, 1573,
-1082, 2508,
-1082, 3392,
-1082, 3874,
-1082, 3949,
+    private static int[] input = new int[]{
+            1082, 4375,
+            1082, 392,
+            1082, 1007,
+            1082, 1573,
+            1082, 2508,
+            1082, 3392,
+            1082, 3874,
+            1082, 3949,
 
-1083, 4149,
-1083, 3949,
+            1083, 4149,
+            1083, 3949,
 
-1242, 1100,
-1242, 3302,
-1242, 3393,
+            1242, 1100,
+            1242, 3302,
+            1242, 3393,
 
-1251, 1100,
-1251, 1618,
-1251, 2013,
-1251, 3302,
+            1251, 1100,
+            1251, 1618,
+            1251, 2013,
+            1251, 3302,
 
-1252, 3302,
+            1252, 3302,
 
-1274, 3302,
-1274, 471,
-1274, 4098,
+            1274, 3302,
+            1274, 471,
+            1274, 4098,
 
-1322, 3091,
-1322, 1453,
+            1322, 3091,
+            1322, 1453,
 
-1323, 2002,
-1323, 1453,
+            1323, 2002,
+            1323, 1453,
 
-1326, 152,
-1326, 1453,
+            1326, 152,
+            1326, 1453,
 
-1375, 3307,
-1375, 381,
-1375, 1277,
+            1375, 3307,
+            1375, 381,
+            1375, 1277,
 
-1378, 2272,
-1378, 2115,
-1378, 3734,
-1378, 4098,
+            1378, 2272,
+            1378, 2115,
+            1378, 3734,
+            1378, 4098,
 
-1711, 3311,
-2167, 4368,
-2167, 492
+            1711, 3311,
+            2167, 4368,
+            2167, 492
     };
 }
